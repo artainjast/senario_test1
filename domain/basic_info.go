172fb01ed711e2a37a6b506af3fe7cb0c044e550
arta Alijan <h.alijani1379@yahoo.com>
2022-03-01 20:56:24 +0330

    finished  basicInfoUseCase

--- domain/basic_info.go
+++ domain/basic_info.go
@@ -1,12 +1,12 @@
 package domain
 
 type City struct {
-	ID         int64  `json:"id"`
-	NAME       string `json:"name"`
-	PROVINCEID int64  `json:"provinceid"`
-	CREATED_AT int64  `json:"created_at"`
-	UPDATED_AT int64  `json:"updated_at"`
-	DELETED_AT int64  `json:"deleted_at"`
+	ID         int64    `json:"id"`
+	NAME       string   `json:"name"`
+	PROVINCE   Province `json:"province"`
+	CREATED_AT int64    `json:"created_at"`
+	UPDATED_AT int64    `json:"updated_at"`
+	DELETED_AT int64    `json:"deleted_at"`
 }
 
 type Province struct {
@@ -17,9 +17,17 @@ type Province struct {
 	DELETED_AT int64  `json:"deleted_at"`
 }
 
-type BasicDataUseCase interface {
-	addProvince(p Province) (err error)
+type BasicInfoUseCase interface {
 	getProvinces(name *string) (p []Province, err error)
+	addProvince(p Province) (err error)
 	updateProvince(id int64, p Province) (err error)
 	deleteProvince(id int64) (err error)
+
+	getCities(name *string) (c []City, err error)
+	addCity(c City) (err error)
+	updateCity(id int, c City) (err error)
+	deleteCity(id int) (err error)
+}
+
+type basicInfoPostgres interface {
 }
